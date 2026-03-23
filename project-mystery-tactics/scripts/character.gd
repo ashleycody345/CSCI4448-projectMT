c11a0b6a529e19bdf947913c933786e9e690df6b
Ashley Cody <123104774+ashleycody345@users.noreply.github.com>
2026-03-23 13:06:48 -0600

    add Character Team enum and team identifier

--- project-mystery-tactics/scripts/character.gd
+++ project-mystery-tactics/scripts/character.gd
@@ -4,9 +4,20 @@ class_name Character
 
 const DEFAULT_HP = 100
 
+enum Team {
+	PLAYER,
+	ENEMY,
+	ALLY,
+	NONE
+}
+
 # member variables
 @export var hp: int = DEFAULT_HP
-@export var is_player: bool = false
+# Controls whether entity can be controlled by player or computer
+@export var is_player: bool = false 	
+# Controls which team a player is on: 0 = player, 1 = enemy, 2 = allied (player), 3 = no team
+@export var team: Team = Team.NONE
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
