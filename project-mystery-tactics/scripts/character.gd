400caa74e8702acd11daa59903bc78d89dc781c2
Ashley Cody <123104774+ashleycody345@users.noreply.github.com>
2026-03-23 13:17:03 -0600

    add Character class stats and defaults

--- project-mystery-tactics/scripts/character.gd
+++ project-mystery-tactics/scripts/character.gd
@@ -3,6 +3,11 @@ extends Node2D
 class_name Character
 
 const DEFAULT_HP = 100
+const DEFAULT_ATK = 1
+const DEFAULT_DMG_TYPE = DamageType.PHYS
+const DEFAULT_DEF = 1
+const DEFAULT_RES = 1
+const DEFAULT_SPD = 5
 
 enum Team {
 	PLAYER,
@@ -11,12 +16,32 @@ enum Team {
 	NONE
 }
 
-# member variables
-@export var hp: int = DEFAULT_HP
+enum DamageType {
+	PHYS,
+	MAG
+}
+
+### member variables
+
+## character properties
+# hit points: upon reaching 0, character dies 
+@export var max_hp: int = DEFAULT_HP
+var hp: int = max_hp
 # Controls whether entity can be controlled by player or computer
 @export var is_player: bool = false 	
 # Controls which team a player is on: 0 = player, 1 = enemy, 2 = allied (player), 3 = no team
 @export var team: Team = Team.NONE
+
+# ATK determines damage output
+@export var ATK: int = DEFAULT_ATK
+# ATK_TYPE determines type of attack
+var ATK_TYPE: DamageType = DEFAULT_DMG_TYPE
+# DEF determines physical damage resistance: damage = ATK - DEF
+@export var DEF: int = DEFAULT_DEF
+# RES determines magic damage resistance: damage = ATK - RES
+@export var RES: int = DEFAULT_RES
+# SPD determines which unit attacks first in combat - if SPD is equal, then attacker goes first
+@export var SPD: int = DEFAULT_SPD
 
 
 # Called when the node enters the scene tree for the first time.
@@ -28,3 +53,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	
 	pass
+
+func isDead() -> bool:
+	return (hp <= 0)
