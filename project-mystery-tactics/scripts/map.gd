287125d9761561c959fc2efa34aa10ed33f9db69
Ashley Cody <123104774+ashleycody345@users.noreply.github.com>
2026-04-16 05:51:22 -0600

    Debug arrow key input processing

--- project-mystery-tactics/scripts/map.gd
+++ project-mystery-tactics/scripts/map.gd
@@ -11,7 +11,15 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	for character in children:
 		set_map_pos(character)
-		
+	process_input()
+
+func process_input() -> void:
+	if(children.size() > 0):
+		if(Input.is_action_just_pressed("debug_left")): children[0].map_pos.x -= 1
+		if(Input.is_action_just_pressed("debug_right")): children[0].map_pos.x += 1
+		if(Input.is_action_just_pressed("debug_down")): children[0].map_pos.y += 1
+		if(Input.is_action_just_pressed("debug_up")): children[0].map_pos.y -= 1
+
 func set_map_pos(character: Character) -> void:
 	print(character.char_name + " position: " + str(character.map_pos.x) + ", " + str(character.map_pos.y))
 	character.position = map_to_local(character.map_pos)
