c08b1b79e8402bea3c4642b7393a9eae559cda52
Ashley Cody <123104774+ashleycody345@users.noreply.github.com>
2026-04-23 19:29:43 -0600

    unit selection on map using mouse

--- project-mystery-tactics/scripts/map.gd
+++ project-mystery-tactics/scripts/map.gd
@@ -1,6 +1,7 @@
 extends TileMapLayer
 
 var children: Array[Character]
+var selected_unit: Character = null
 
 func _ready() -> void:
 	var all_children = self.get_children()
@@ -14,12 +15,24 @@ func _process(delta: float) -> void:
 	process_input()
 
 func process_input() -> void:
-	if(children.size() > 0):
-		if(Input.is_action_just_pressed("debug_left")): children[0].map_pos.x -= 1
-		if(Input.is_action_just_pressed("debug_right")): children[0].map_pos.x += 1
-		if(Input.is_action_just_pressed("debug_down")): children[0].map_pos.y += 1
-		if(Input.is_action_just_pressed("debug_up")): children[0].map_pos.y -= 1
+	# child selection
+	var child_hovered_over: Character = select_child_hovered_over()
+	if(child_hovered_over == null): return
+	
+	if(Input.is_action_just_pressed("mouse_left_click")):
+		selected_unit = child_hovered_over
+		print("selected unit: " + selected_unit.char_name)
 
 func set_map_pos(character: Character) -> void:
-	print(character.char_name + " position: " + str(character.map_pos.x) + ", " + str(character.map_pos.y))
+	#print(character.char_name + " position: " + str(character.map_pos.x) + ", " + str(character.map_pos.y))
 	character.position = map_to_local(character.map_pos)
+
+func select_child_hovered_over() -> Character:
+	var map_tile_hovered: Vector2i = local_to_map(get_local_mouse_position())
+	var char_hovered: Character
+	var char_name: String 
+	for child in children:
+		if(child.map_pos == map_tile_hovered):
+			char_hovered = child
+			break
+	return char_hovered
