226f5389a2729dc20b3f14d99f93d9ca6c0bf3ac
Ashley Cody <123104774+ashleycody345@users.noreply.github.com>
2026-04-25 11:42:56 -0600

    Add character builder

---
+++ project-mystery-tactics/tests/scripts/test_items.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+class_name ItemTest
+
+var character1: DecoratedCharacter = 
+
+var test_count = 1
+
+func _ready() -> void:
+	run_tests()
+	
+	
+func run_tests() -> bool:
+	return false
